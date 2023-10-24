ef9a4b13535295cf11fe7675c39f8aebc7e1ea87
Kacper <51298603+xjustkacper@users.noreply.github.com>
2023-10-24 18:33:59 +0200

    Hello World

---
+++ src/test/java/com/ngrbooks/ngrbooks/NgrbooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ngrbooks.ngrbooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NgrbooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
